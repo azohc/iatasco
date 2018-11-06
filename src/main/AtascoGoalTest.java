2e6b52745c17f31270b862a6eb8a2fe6a6fb1190
chosa <chosa@tocheeba>
2018-11-06 12:33:10 +0100

    Cosillas añadidas en la doble clase

--- src/main/AtascoGoalTest.java
+++ src/main/AtascoGoalTest.java
@@ -1,5 +1,22 @@
 package main;
 
-public class AtascoGoalTest {
+import aima.core.search.framework.problem.GoalTest;
 
+public class AtascoGoalTest implements GoalTest{
+
+	
+	@Override
+	public boolean isGoalState(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean GoalTest(EstadoAtasco e) {
+		for(int i = 0; i < e.getTam(); i++) 
+			if(e.getTablero()[i][e.getTam() - 1] == 'z')
+				return true;
+		
+		return false;
+	}
 }
--- src/main/EstadoAtasco.java
+++ src/main/EstadoAtasco.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import aima.core.agent.Action; 
 import aima.core.agent.impl.DynamicAction;
+import aima.core.util.datastructure.XYLocation;
 
 public class EstadoAtasco {
 
@@ -21,12 +22,9 @@ public class EstadoAtasco {
 	private static final char PARED = '#';
 	private static final char META = '@';
 	private static final char HUECO = '.';
-	
-	
 
 	public static Action palante = new DynamicAction("PAL");
 	public static Action patras = new DynamicAction("PAT");
-	
 	
 	
 	public EstadoAtasco() {
@@ -35,7 +33,13 @@ public class EstadoAtasco {
 	}
 	
 	public EstadoAtasco(EstadoAtasco e) {
-		_tablero = e._tablero;
+		//_tablero = e._tablero; //TODO CHECK
+		
+		_tablero = new char[e._tam][e._tam];
+		
+		for(int i = 0; i < e._tam; i++)
+			System.arraycopy(e._tablero[i], 0, _tablero[i], 0, e._tam);
+		
 		_tam = e._tam;
 	}
 	
@@ -120,7 +124,7 @@ public class EstadoAtasco {
 			i = y + 1;
 			while(_tablero[x][i++] == c)	//TODO check ? oob
 						
-			if(y > 1 &&_tablero[x][y-1] == HUECO) {
+			if(y > 1 && _tablero[x][y-1] == HUECO) {
 				_tablero[x][y - 1] = c;
 				_tablero[x][i - 1] = HUECO;
 			}
@@ -129,21 +133,72 @@ public class EstadoAtasco {
 			i = x + 1;
 			while(_tablero[i++][y] == c)	//TODO check ? oob
 			
-			if(x > 1 &&_tablero[x-1][y] == HUECO) {
+			if(x > 1 && _tablero[x-1][y] == HUECO) {
 				_tablero[x - 1][y] = c;
 				_tablero[i - 1][y] = HUECO;
 			}
 		}
 	}
 	
-	
-	public boolean GoalTest(EstadoAtasco e) {
-		for(int i = 0; i < _tam; i++) 
-			if(_tablero[i][_tam] == 'z')
-				return true;
+	public boolean canMoveCar(Action where, char car) {
+		if(car == PARED || car == META || car == HUECO)
+			return false;
+		XYLocation carLoc = getPositionOf(car);
+		boolean esHorizontal = Character.isLowerCase(car);
+		int i = 0, x = carLoc.getXCoOrdinate(), y = carLoc.getYCoOrdinate();
 		
+		if(where.equals(palante)) {
+			if(esHorizontal) {
+				i = y + 1;
+				while(_tablero[x][i++] == car)	//TODO check
+							
+				if(i < _tam - 1 && _tablero[x][i] == HUECO) 
+					return true;
+			}
+			else {
+				i = x + 1;
+				while(_tablero[i++][y] == car)	//TODO check
+				
+				if(i < _tam - 1 && _tablero[i][y] == HUECO) 
+					return true;
+			}
+		}
+		else {
+			if(esHorizontal) 
+				if(y > 1 && _tablero[x][y-1] == HUECO) 
+					return true;
+			else 
+				if(x > 1 && _tablero[x-1][y] == HUECO) 
+					return true;
+		}
 		return false;
 	}
+	
+	public XYLocation getPositionOf(char c) {
+		int x = 0, y = 0;
+		
+		while(x < _tam && _tablero[x][y] != c) {
+			while(y < _tam && _tablero[x][y] != c) {
+				y++;
+			}
+			x++;
+		}
+		return new XYLocation(x, y);		
+	}
+	
+	
+	public char[][] getTablero(){
+		return _tablero;
+	}
+
+	public int getTam() {
+		return _tam;
+	}
+
+	public void setTam(int _tam) {
+		this._tam = _tam;
+	}
+	
 	
 	
 }
